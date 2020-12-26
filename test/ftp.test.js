04448ad32da96d327619e7841fba829bc474b5c6
ThomasCho <bb371331811@msn.cn>
2020-12-26 12:01:14 +0800

    [update] 补全部分单测
    改动：
    1. 补全了15个单测，包括接口缺失、上传、列举、删除等用例。
    
    测试：
    1. 15个用例都成功。

--- test/ftp.test.js
+++ test/ftp.test.js
@@ -1,13 +1,29 @@
 const ftp = require('ftp');
-const FTP = require('../ftp.js');
+const FTP = require('../lib/ftp.js');
+const BaseUploader = require('../lib/base-uploader.js');
+
+let opt = {
+  host: '47.107.157.97',
+  port: '21',
+  user: 'ftp',
+  password: 'Admin@123',
+  root: '/czm/',
+};
 
 jest.mock('ftp', () => {
   return jest.fn(() => ({
     connect: jest.fn(() => Promise),
     delete: jest.fn((path, cb) => {
+      if (path === (opt.root + 'error')) {
+        return cb && cb('error');
+      }
       cb && cb();
     }),
     put: jest.fn((file, remoteFile, cb) => {
+      // 假设上传 error.png 会上传失败
+      if (file === 'error.png') {
+        return cb && cb('error');
+      }
       cb && cb();
     }),
     list: jest.fn((path, cb) => {
@@ -29,22 +45,97 @@ beforeEach(() => {
   ftp.mockClear();
 });
 
-let opt = {
-  host: '47.107.157.97',
-  port: '21',
-  user: 'ftp',
-  password: 'Admin@123',
-  root: '/czm/',
-};
+describe('Suite：接口缺失', () => {
+  test('[Constructor] 构造器未传递', async () => {
+    class FakeFTP extends BaseUploader {
+      constructor(options) {
+        super(options);
+        expect(() => {
+          this.initClient();
+        }).toThrowError('not overwritten');
+      }
+    }
+  });
 
-describe('ftp test', () => {
+  test('[Method] upload 未被复写', async () => {
+    class FakeFTP extends BaseUploader {
+      constructor(options) {
+        super(options);
+        this.baseConstructor = ftp;
+        this.initClient();
+      }
+    }
+
+    const ftpClient = new FakeFTP(opt);
+    await ftpClient.connect();
+
+    expect(() => {
+      ftpClient.upload('xxx');
+    }).toThrowError('not overwritten');
+  });
+
+  test('[Method] listFile 未被复写', async () => {
+    class FakeFTP extends BaseUploader {
+      constructor(options) {
+        super(options);
+        this.baseConstructor = ftp;
+        this.initClient();
+      }
+    }
+
+    const ftpClient = new FakeFTP(opt);
+    await ftpClient.connect();
+
+    expect(() => {
+      ftpClient.listFile();
+    }).toThrowError('not overwritten');
+  });
+
+  test('[Method] download 未被复写', async () => {
+    class FakeFTP extends BaseUploader {
+      constructor(options) {
+        super(options);
+        this.baseConstructor = ftp;
+        this.initClient();
+      }
+    }
+
+    const ftpClient = new FakeFTP(opt);
+    await ftpClient.connect();
+
+    expect(() => {
+      ftpClient.download('xxx');
+    }).toThrowError('not overwritten');
+  });
+
+  test('[Method] deleteFile 未被复写', async () => {
+    class FakeFTP extends BaseUploader {
+      constructor(options) {
+        super(options);
+        this.baseConstructor = ftp;
+        this.initClient();
+      }
+    }
+
+    const ftpClient = new FakeFTP(opt);
+    await ftpClient.connect();
+
+    expect(() => {
+      ftpClient.deleteFile('xxx');
+    }).toThrowError('not overwritten');
+  });
+});
+
+describe('Suite：测试连接', () => {
   test('测试连接', async () => {
     const ftpClient = new FTP(opt);
     const options = await ftpClient.connect();
     ftpClient.destroy();
     expect(options).toEqual(opt);
   });
+});
 
+describe('Suite：列举文件', () => {
   test('列举文件', async () => {
     const ftpClient = new FTP(opt);
     await ftpClient.connect();
@@ -53,7 +144,30 @@ describe('ftp test', () => {
     expect(files).toBeInstanceOf(Array);
   });
 
-  test('删除文件', async () => {
+  test('列举文件，带路径', async () => {
+    let path = '/imgs';
+
+    const ftpClient = new FTP(opt);
+    await ftpClient.connect();
+
+    const files = await ftpClient.listFile(path);
+    expect(files).toBeInstanceOf(Array);
+  });
+});
+
+describe('Suite：删除功能', () => {
+  test('删除文件（单文件）', async () => {
+    let testFiles = ['a'];
+
+    const ftpClient = new FTP(opt);
+    await ftpClient.connect();
+
+    let files = await ftpClient.deleteFile(testFiles);
+    files = files.map(file => file.value);
+    expect(files).toEqual(testFiles);
+  });
+
+  test('删除文件（多文件）', async () => {
     let testFiles = ['a', 'b'];
 
     const ftpClient = new FTP(opt);
@@ -64,29 +178,77 @@ describe('ftp test', () => {
     expect(files).toEqual(testFiles);
   });
 
-  // test('上传文件', async () => {
-  //   let testFiles = ['cat.png', 'coffee.png'];
+  test('删除文件（失败）', async () => {
+    let testFiles = ['error'];
 
-  //   const ftpClient = new FTP(opt);
-  //   await ftpClient.connect();
+    const ftpClient = new FTP(opt);
+    await ftpClient.connect();
 
-  //   let files = await ftpClient.upload(testFiles);
-  //   files = files.map(file => file.value);
+    let files = await ftpClient.deleteFile(testFiles);
+    files = files.map(file => file.reason);
+    expect(files).toEqual(testFiles);
+  });
+});
 
-  //   console.log(files);
-  //   expect(files).toEqual(testFiles);
-  // });
+describe('Suite：上传功能', () => {
+  test('上传文件（单文件）', async () => {
+    let testFiles = ['cat.png'];
 
-  // test('下载文件', async () => {
-  //   let testFiles = ['cat.png', 'coffee.png'];
+    const ftpClient = new FTP(opt);
+    await ftpClient.connect();
 
-  //   const ftpClient = new FTP(opt);
-  //   await ftpClient.connect();
+    let files = await ftpClient.upload(testFiles);
 
-  //   let files = await ftpClient.download(testFiles);
-  //   files = files.map(file => file.value);
+    files = files
+      .filter(pro => pro.status === 'fulfilled')
+      .map(pro => pro.value);
 
-  //   console.log(files);
-  //   expect(files).toEqual(testFiles);
-  // });
+    expect(files).toEqual(testFiles);
+  });
+
+  test('上传文件（多文件）', async () => {
+    let testFiles = ['cat.png', 'coffee.png'];
+
+    const ftpClient = new FTP(opt);
+    await ftpClient.connect();
+
+    let files = await ftpClient.upload(testFiles);
+
+    files = files
+      .filter(pro => pro.status === 'fulfilled')
+      .map(pro => pro.value);
+
+    expect(files).toEqual(testFiles);
+  });
+
+  test('上传文件（失败：文件不存在）', async () => {
+    let testFiles = ['fake-file.png'];
+
+    const ftpClient = new FTP(opt);
+    await ftpClient.connect();
+
+    let files = await ftpClient.upload(testFiles);
+
+    files = files
+      .filter(pro => pro.status === 'rejected')
+      .map(pro => pro.reason);
+
+    expect(files).toEqual(['file not existed']);
+  });
+
+
+  test('上传文件（失败：文件存在，但上传失败）', async () => {
+    let testFiles = ['error.png'];
+
+    const ftpClient = new FTP(opt);
+    await ftpClient.connect();
+
+    let files = await ftpClient.upload(testFiles);
+
+    files = files
+      .filter(pro => pro.status === 'rejected')
+      .map(pro => pro.reason);
+
+    expect(files).toEqual(['error']);
+  });
 });
